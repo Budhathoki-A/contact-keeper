4fec490b813beafe85c6ce5a26b16d835ca5a7d9
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-05-20 20:00:23 +1000

    User schema Added

--- routes/users.js
+++ routes/users.js
@@ -25,14 +25,30 @@ router.post(
       'please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // res.send(req.body);  res.body gives the data that sent to the route which
     // in case will be the email, password and name.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+    } catch (err) {}
   }
 );
 
